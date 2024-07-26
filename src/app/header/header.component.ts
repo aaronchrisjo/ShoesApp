1179c32974d01a20f4b1e9d27d4463814d672ec9
aaron-gcl-bi <aaron@gcl-bi.com>
2024-07-26 16:19:01 +0530

    Conditionally rendered the login and logout buttons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,18 +2,29 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { AuthenticationGuardService } from '../authentication.guard.service';
 import { AuthenticationService } from '../authentication.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   logoUrl: string = 'assets/logo.png';
+  isAuthenticated : boolean =false;
 
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(private authenticationService: AuthenticationService,
+    public router:Router
+  ) {}
+
+  ngOnInit():void{
+    this.authenticationService.isLoggedIn().subscribe(status=>{
+      this.isAuthenticated = status;
+    })
+  }
 
   logout():void{
     this.authenticationService.logout();
